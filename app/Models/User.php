69e4027abdd66a11dee22a225e84dcfa0272d670
Yudita Pratama <wyudita321@gmail.com>
2024-01-11 10:49:49 +0800

    Migration Database

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,10 +17,13 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
+    
+     protected $table = 'users';
+     protected $fillable = [
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -30,16 +33,9 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
+        
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    const CREATED_AT = 'creation_at';
+    const UPDATED_AT = 'updated_at';
 }
